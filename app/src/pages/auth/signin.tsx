8e61304ef4f255ee017c4b41ccf6989aec0f3fc7
JustinMatthewNewman <newma4jm@dukes.jmu.edu>
2023-02-07 21:30:17 -0500

    next-auth

--- app/src/pages/auth/signin.tsx
+++ app/src/pages/auth/signin.tsx
@@ -1,38 +1,40 @@
-import React from 'react'
 import { getProviders, signIn as SignIntoProvider } from "next-auth/react";
-import Header from '../../components/Header'
+import Header from "../../components/Header";
 
-function signIn( { providers }: { providers: Object } ) {
-  console.log(providers)
+function signIn({ providers }) {
   return (
     <>
-    <div className="h-100 bg-white">
+      <Header />
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-55 px-14 text-center">
+        <img className="w-80" src="http://links.papareact.com/ocw" alt="" />
+        <p className="font-xs italic">
+          This is not a REAL app, it is built for educational purposes only
+        </p>
 
-    <Header />
-    <div className="flex flex-col items-center h-100 justify-center mt-10 py-20 ">
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button className="p-3 bg-blue-500 rounded-lg text-white" onClick={() => SignIntoProvider(provider.id, { callbackUrl: "/"})}>
-            Sign in with {provider.name}
-          </button>
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                className="p-3 bg-blue-500 rounded-lg text-white"
+                onClick={() => SignIntoProvider(provider.id, { callbackUrl: "/"})}
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
-
-    </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-// Server side render
 export async function getServerSideProps() {
-    const providers = await getProviders()
-
-    return {
-        props: {
-            providers,
-        },
-    };
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
 }
 
 export default signIn;
