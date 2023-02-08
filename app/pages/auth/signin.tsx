022ff434be40e28273457ad4fa9439929ae86471
JustinMatthewNewman <newma4jm@dukes.jmu.edu>
2023-02-07 19:52:21 -0500

    Fixed build error

--- app/pages/auth/signin.tsx
+++ app/pages/auth/signin.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { getProviders, signIn as SignIntoProvider } from "next-auth/react";
-import Header from '../../components/Header.js'
+import Header from "../../components/Header.js";
 
 function signIn( { providers }: { providers: Object } ) {
   return (
