0f993ee89575a74127623c3b87e6910dfb03aae2
JustinMatthewNewman <newma4jm@dukes.jmu.edu>
2023-02-08 20:22:55 -0500

    loading screen

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -3,16 +3,50 @@ import type { AppProps } from 'next/app'
 
 import { SessionProvider } from 'next-auth/react'
 import { RecoilRoot } from "recoil"
+import HashLoader from "react-spinners/HashLoader";
+
+import React, {useState, useEffect, CSSProperties} from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(false);
+  const override: CSSProperties = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "red",
+    height: "1000px",
+    width: "1000px",
+  };
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+       setLoading(false) 
+    }, 2000)
+  }, [])
   return (
-    <SessionProvider>
-      <RecoilRoot>
+    <div className="bg-slate-800 h-full w-full">
 
-      <Component {...pageProps} />
-      </RecoilRoot>
-
-    </SessionProvider>
+      {
+        loading ?
+  
+        <HashLoader
+          color={"#853ead"}
+          loading={loading}
+          cssOverride={override}
+          size={150}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+        :
+      <SessionProvider>
+        <RecoilRoot>
+  
+        <Component {...pageProps} />
+        </RecoilRoot>
+  
+      </SessionProvider>
+      }
+    </div>
   )
   
 }
