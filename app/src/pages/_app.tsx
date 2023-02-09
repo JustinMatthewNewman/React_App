5d14b7551b04597deef424d98e69cfd3cb4dac14
JustinMatthewNewman <newma4jm@dukes.jmu.edu>
2023-02-08 20:32:39 -0500

    regression test

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -3,49 +3,16 @@ import type { AppProps } from 'next/app'
 
 import { SessionProvider } from 'next-auth/react'
 import { RecoilRoot } from "recoil"
-import HashLoader from "react-spinners/HashLoader";
-
-import React, {useState, useEffect, CSSProperties} from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [loading, setLoading] = useState(false);
-  const override: CSSProperties = {
-    display: "block",
-    margin: "0 auto",
-    borderColor: "red",
-    height: "1000px",
-  };
-
-  useEffect(() => {
-    setLoading(true)
-    setTimeout(() => {
-       setLoading(false) 
-    }, 2000)
-  }, [])
   return (
-    <div className="bg-slate-800 h-full w-full overflow-y-scroll scrollbar-hide">
+    <SessionProvider>
+      <RecoilRoot>
 
-      {
-        loading ?
-  
-        <HashLoader
-          color={"#853ead"}
-          loading={loading}
-          cssOverride={override}
-          size={150}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-        />
-        :
-      <SessionProvider>
-        <RecoilRoot>
-  
-        <Component {...pageProps} />
-        </RecoilRoot>
-  
-      </SessionProvider>
-      }
-    </div>
+      <Component {...pageProps} />
+      </RecoilRoot>
+
+    </SessionProvider>
   )
   
 }
