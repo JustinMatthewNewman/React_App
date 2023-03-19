e3c0a00ce21c8c825a5f99c68030006e3fd394fd
JustinMatthewNewman <newma4jm@dukes.jmu.edu>
2023-03-19 02:42:12 -0400

    Signin page

--- app/src/pages/auth/signin.tsx
+++ app/src/pages/auth/signin.tsx
@@ -4,6 +4,7 @@ import Header from "../../components/Header";
 
 function SignIn({ providers }: { providers: Object }) {
   const [welcomeText, setWelcomeText] = useState("");
+  //const [Globe, setGlobe] = useState(null);
 
   useEffect(() => {
     const welcomeString =
@@ -20,26 +21,43 @@ function SignIn({ providers }: { providers: Object }) {
     return () => clearInterval(typeEffect);
   }, []);
 
+  // useEffect(() => {
+  //   import("react-globe.gl").then((module1) => {
+  //     setGlobe(module1.default);
+  //   });
+  // }, []);
+
+  //if (!Globe) return null;
+
   return (
     <>
       <Header />
       <div className="h-screen bg-slate-800 flex flex-col items-center justify-center min-h-screen py-2 -mt-55 px-14 text-center">
-        <img className="w-80" src="" alt="" />
-        <p className="font-md italic text-white">{welcomeText}</p>
-
-        <div className="mt-40">
-          {Object.values(providers).map((provider) => (
-            <div key={provider.name}>
-              <button
-                className="p-3 bg-blue-500 rounded-lg text-white"
-                onClick={() =>
-                  SignIntoProvider(provider.id, { callbackUrl: "/" })
-                }
-              >
-                Sign in with {provider.name}
-              </button>
-            </div>
-          ))}
+        <div className="mt-16 relative">
+          {/* <div className="mt-40 absolute top-0 left-0 right-0 z-50"> */}
+            {Object.values(providers).map((provider) => (
+              <div key={provider.name}>
+                <button
+                  style={{ pointerEvents: "auto" }}
+                  className="p-3 bg-white rounded-lg text-black"
+                  onClick={() =>
+                    SignIntoProvider(provider.id, { callbackUrl: "/" })
+                  }
+                >
+                  <div className="flex items-center">
+                    <img className="h-10 w-10" src="/google.png"></img> Sign in
+                    with {provider.name}
+                  </div>
+                </button>
+                <p className="font-md italic text-white mt-16">{welcomeText}</p>
+              </div>
+            ))}
+          {/* </div> */}
+          {/* <Globe
+            height="300px"
+            globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+            backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+          /> */}
         </div>
       </div>
     </>
