83b62f590539a91ffbaa9e1285c1f35b986a823f
Justin Newman <justin.newman@metalinguist.com>
2023-07-09 15:22:15 -0400

    loading

--- app/src/components/Posts.js
+++ app/src/components/Posts.js
@@ -1,116 +1,44 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Post from './Post.js';
-import { db } from '../../firebase.js';
-import { collection, query, orderBy, limit, startAfter, onSnapshot } from '@firebase/firestore';
-
-import { TailSpin } from 'react-loader-spinner';
+import React from 'react'
+import Post from './Post.js'
+import { useState, useEffect } from 'react'
+import { db, storage } from "../../firebase.js"
+import { collection, onSnapshot, orderBy, query } from "@firebase/firestore"
+import { TailSpin } from  'react-loader-spinner'
 
 
 function Posts() {
   const [posts, setPosts] = useState([]);
-  const [lastPost, setLastPost] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const observerRef = useRef(null);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'), limit(2));
-      const snapshot = await onSnapshot(q, (querySnapshot) => {
-        const newPosts = querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        const lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1];
-        setPosts(newPosts);
-        setLastPost(lastVisible);
-        setLoading(false);
-      });
-
-      return () => {
-        snapshot();
-      };
-    };
-
-    fetchPosts();
-  }, []);
-
-  useEffect(() => {
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 1.0,
-    };
-
-    const handleIntersection = (entries) => {
-      const target = entries[0];
-      if (target.isIntersecting) {
-        loadMorePosts();
-      }
-    };
-
-    observerRef.current = new IntersectionObserver(handleIntersection, options);
-
-    if (observerRef.current) {
-      observerRef.current.observe(document.getElementById('observer'));
-    }
-
-    return () => {
-      if (observerRef.current) {
-        observerRef.current.disconnect();
-      }
-    };
-  }, [posts]);
-
-  const loadMorePosts = async () => {
-    if (lastPost) {
-      setLoading(true);
-      const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'), startAfter(lastPost), limit(2));
-      const snapshot = await onSnapshot(q, (querySnapshot) => {
-        const newPosts = querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        const lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1];
-        setPosts((prevPosts) => [...prevPosts, ...newPosts]);
-        setLastPost(lastVisible);
-        setLoading(false);
-      });
-
-      return () => {
-        snapshot();
-      };
-    }
-  };
-
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => 
+      onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), (snapshot) => {
+          setPosts(snapshot.docs);
+          setIsLoading(false);
+      }),
+[db]);
+console.log(posts)
+if(isLoading) {
   return (
-    <div>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          id={post.id}
-          username={post.username}
-          userimage={post.profileImg}
-          media={post.image}
-          caption={post.caption}
-          user_id={post.user_id}
-        />
-      ))}
-      {loading && (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '128px' }}>
-          <TailSpin
-            height={80}
-            width={80}
-            color="#FFF"
-            ariaLabel="TailSpin-loading"
-            wrapperStyle={{}}
-            wrapperClassName=""
-            visible={true}
-          />
-        </div>
-      )}
-      <div id="observer"></div>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <TailSpin color="#00BFFF" height={80} width={80} />
     </div>
   );
 }
 
-export default Posts;
+  return (
+    <div>
+      {posts.map((post) => (
+        <Post 
+        key={post.id}
+        id={post.id}
+        username={post.data().username}
+        userimage={post.data().profileImg}
+        media={post.data().image}
+        caption={post.data().caption}
+        user_id={post.data().user_id}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Posts
